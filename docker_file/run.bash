f237feecaff216ed9036a14499b8f2153c4451cb
apola <yan_1whun1@hotmail.com>
2024-10-09 19:18:02 +0400

    cuda

--- docker_file/run.bash
+++ docker_file/run.bash
@@ -7,17 +7,20 @@ xinput --list | grep keyboard
 echo -n "Enter the keyboard id: " 
 read keyboard_id
 
-docker run -it --rm \
+docker run -it\
   --privileged \
   --network=host \
+  --runtime=nvidia \
   --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
+  --env="NVIDIA_VISIBLE_DEVICES=all"\
+  --env="NVIDIA_DRIVER_CAPABILITIES=all"\
   --env="keyboard_id=${keyboard_id}" \
+  --volume="/dev:/dev" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --volume="${HOME}/projectLiLi:/root/projectLiLi" \
   --name="projectLiLi" \
   projectlili:latest \
   sh -c "python3 /root/projectLiLi/src/yolo.py"
-
 
 
